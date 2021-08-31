c5f877c6831454175c5318bc192a1e6f3aafbfe3
user <user@userui-MacBookPro.local>
2021-08-31 19:13:47 +0900

    BoardsNew-style_update_divide

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,194 +1,31 @@
-import styled from "@emotion/styled";
+import{
+  AddBox,
+  AddBoxWrapper,
+  Address,
+  AddressWrapper,
+  AddWrapper,
+  ButtonWrapper,
+  CancelBtn,
+  Check,
+  ContentInput,
+  Input,
+  InputWrapper,
+  Label,
+  RadioInput,
+  RadioLabel,
+  SelectWrapper,
+  SubmitBtn,
+  Title,
+  Vector,
+  Wrapper,
+  WriterInput,
+  WriterWrapper,
+  ZipcodeBtn,
+  ZipcodeInput,
+  ZipcodeWrapper
+} from '../../../styles/BoardsNew-style'
 
 export default function BoardsNewPage() {
-
-  const Wrapper = styled.div`
-    width: 1200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-shadow: 0px 4px 20px;
-    padding-top: 90px;
-    padding-bottom: 100px;
-    margin: 100px;
-  `
-
-  const Title = styled.div`
-    font-size: 36px;
-    font-weight: bold;
-  `
-
-  const WriterWrapper = styled.div`
-    width: 996px;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 40px;
-  `
-
-  const InputWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding-top: 40px;
-  `
-
-  const Label = styled.div`
-    font-size: 16px;
-    margin-bottom: 12px;
-    margin-left: 1px;
-    font-weight: 500;
-  `
-
-  const Check = styled.span`
-    color: #FFD600;
-  `
-
-  const WriterInput = styled.input`
-    border: 1px solid #BDBDBD;
-    width: 486px;
-    height: 52px;
-    padding-left: 15px;
-    font-size: 16px;
-  `
-
-  const Input = styled.input`
-    border: 1px solid #BDBDBD;
-    width: 996px;
-    height: 52px;
-    padding-left:15px;
-    font-size: 16px;
-    `
-
-  const ContentInput = styled.textarea`
-    border: 1px solid #BDBDBD;
-    width: 996px;
-    height: 480px;
-    padding-top: 16px;
-    padding-left:15px;
-    font-size: 16px;
-    `
-
-  const AddressWrapper = styled.div`
-    height: 244px;
-    display: flex;
-    flex-direction: column;
-    /* justify-content: space-between; */
-    padding-top: 16px;
-  `
-
-  const ZipcodeWrapper = styled.div`
-    width: 217px;
-    display: flex;
-    justify-content: space-between;
-  `
-
-  const ZipcodeInput = styled.input`
-    border: 1px solid #BDBDBD;
-    width: 77px;
-    height: 52px;
-    padding-left:15px;
-    font-size: 16px;
-  `
-
-  const ZipcodeBtn = styled.button`
-    background-color: black;
-    width: 124px;
-    height: 52px;
-    color: white;
-    font-weight: 500;
-    font-size: 16px;
-    cursor: pointer;
-  `
-  const Address = styled.input`
-    border: 1px solid #BDBDBD;
-    width: 996px;
-    height: 52px;
-    padding-left:15px;
-    font-size: 16px;
-    margin-top: 16px;
-  `
-
-  const AddWrapper = styled.div`
-    width: 996px;
-    padding-top: 40px;
-  `
-
-  const AddBoxWrapper = styled.div`
-    display: flex;
-    width: 282px;
-    display:flex;
-    justify-content: space-between;
-  `
-
-  const AddBox = styled.button`
-    width: 78px;
-    height: 78px;
-    background-color: #BDBDBD;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-size: 12px;
-    color: #4F4F4F;
-    cursor: pointer;
-    border: none;
-  `
-
-  const Vector = styled.img`
-    margin-bottom: 5px;
-  `
-
-  const SelectWrapper = styled.div`
-    padding-top: 40px;
-    width: 996px;
-  `
-
-  // const RadioWrapper = styled.div`
-  //   height: 24px;
-  //   display: flex;
-  //   align-items: center;
-  // `
-
-  const RadioInput = styled.input`
-    width: 15px;
-    height: 15px;
-    background: #FFFFFF;
-    border: 1px solid #FFD600;
-    cursor: pointer;
-  `
-
-  const RadioLabel = styled.label`
-    margin-left: 10px;
-    margin-right: 22px;
-    font-size: 16px;
-    font-weight: 500;
-    /* padding-bottom: 1px; */
-  `
-
-  const CancelBtn = styled.button`
-    background-color: #BDBDBD;
-    width:179px;
-    height: 52px;
-    border: none;
-    font-size: 16px;
-    font-weight: 500;
-    color: #4F4F4F;
-    cursor: pointer;
-  `
-  const SubmitBtn = styled.button`
-    background-color: #FFD600;
-    width:179px;
-    height: 52px;
-    border: none;
-    font-size: 16px;
-    font-weight: 500;
-    cursor: pointer;
-    `
-  const ButtonWrapper = styled.div`
-    width: 382px;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 40px;
-  `
 
   return (
     <Wrapper>
