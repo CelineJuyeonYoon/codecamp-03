1b3149962f1a0ffcce91b5525ee5b30a1cc05372
user <filament3@gmail.com>
2021-10-19 19:13:57 +0900

    권한분기 새로고침시 accessToken 리셋되는 문제 수정

--- freeboard_frontend/pages/src/components/commons/hocs/withAuth.tsx
+++ freeboard_frontend/pages/src/components/commons/hocs/withAuth.tsx
@@ -4,10 +4,13 @@ import { GlobalContext } from "../../../../_app";
 
 export const withAuth = (Component) => (props) => {
   const router = useRouter();
-  const { accessToken } = useContext(GlobalContext);
+  const { accessToken, setAccessToken } = useContext(GlobalContext);
 
   useEffect(() => {
-    if (!accessToken) {
+    const token = localStorage.getItem("accessToken") || "";
+    setAccessToken(token);
+    if (!token) {
+      // if(!accessToken)으로 하면, accessToken을 불러오기 전에 렌더되어 accessToken이 있어도 조건문이 실행돼버림.
       alert("로그인을 해주세요!");
       router.push("/login");
     }
