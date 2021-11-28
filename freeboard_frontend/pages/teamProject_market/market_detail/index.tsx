e339737e2923d12b971faedf2862365f916cb1fb
user <filament3@gmail.com>
2021-11-28 13:37:00 +0900

    Update : 파일정리

--- freeboard_frontend/pages/teamProject_market/market_detail/index.tsx
+++
@@ -1,5 +0,0 @@
-import MarketDetail from "../../src/components/units/posh/detail/MarketDetail.container";
-
-export default function MarketDetailPage() {
-  return <MarketDetail />;
-}
--- freeboard_frontend/pages/teamProject_market/market_write/index.tsx
+++
@@ -1,5 +0,0 @@
-import MarketWrite from "../../src/components/units/posh/write/MarketWrite.container";
-
-export default function MarketWritePage() {
-  return <MarketWrite />;
-}
