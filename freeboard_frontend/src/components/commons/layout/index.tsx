eada05a3ad4da26ad8a37ede5700a96f3656e15b
user <filament3@gmail.com>
2022-01-12 23:53:47 +0900

    Update : 마이페이지 사이드바 삭제

--- freeboard_frontend/src/components/commons/layout/index.tsx
+++ freeboard_frontend/src/components/commons/layout/index.tsx
@@ -1,15 +1,10 @@
 import styled from "@emotion/styled";
 import LayoutHeader from "./header/LayoutHeader.container";
 import LayoutBanner from "./banner/LayoutBanner.container";
-// import LayoutNavigation from "./navigation/LayoutNavagation.container";
 import LayoutFooter from "./footer/LayoutFooter.container";
-import SideBar from "./sidebar/SideBar.container";
 import { useRouter } from "next/dist/client/router";
 
-const Wrapper = styled.div`
-  /* display: flex;
-  flex-direction: column; */
-`;
+const Wrapper = styled.div``;
 
 const Body = styled.div`
   height: 55%;
@@ -22,19 +17,15 @@ const Body = styled.div`
 `;
 
 const HIDDEN_PAGE = ["/"];
-const MARKET_MAIN = ["/mypage"];
 
 export default function Layout(props: any) {
   const router = useRouter();
   const isHiddenPage = HIDDEN_PAGE.includes(router.pathname);
-  const isMarketMain = MARKET_MAIN.includes(router.pathname);
 
   return (
     <Wrapper>
       {!isHiddenPage && <LayoutHeader />}
       {!isHiddenPage && <LayoutBanner />}
-      {/* {!isHiddenPage && <LayoutNavigation />} */}
-      {isMarketMain && <SideBar />}
       <Body>{props.children}</Body>
       {!isHiddenPage && <LayoutFooter />}
     </Wrapper>
